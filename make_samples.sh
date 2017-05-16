ed6efa33ede7494d5c4f92aea31d7bd0793a1a57
Nick Bennett <nicholas.bennett@kellogg.northwestern.edu>
2017-05-16 13:27:18 -0500

    Add script to create sample files of resultant CSVs.

---
+++ make_samples.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+head -n 1000 yelp_academic_dataset_business.csv > ./data_samples/yelp_academic_dataset_business-head1000.csv
+head -n 1000 yelp_academic_dataset_checkin.csv > ./data_samples/yelp_academic_dataset_checkin-head1000.csv
+head -n 1000 yelp_academic_dataset_review.csv > ./data_samples/yelp_academic_dataset_review-head1000.csv
+head -n 1000 yelp_academic_dataset_tip.csv > ./data_samples/yelp_academic_dataset_tip-head1000.csv
+head -n 1000 yelp_academic_dataset_user.csv > ./data_samples/yelp_academic_dataset_user-head1000.csv
+zip yelp_academic_dataset-CSV-samples.zip ./data_samples/*.csv
