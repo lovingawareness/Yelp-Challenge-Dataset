71a396a967b69a264fa0ef5a8f72d3d26088dc52
Nick Bennett <nicholas.bennett@kellogg.northwestern.edu>
2017-05-22 16:16:10 -0500

    Script to merge the yelp_academic_dataset_business-restaurants.csv with yelp_academic_dataset_review-wordcount.csv on business_id.

---
+++ merge_business_review.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+import unicodecsv as csv
+from collections import defaultdict
+
+with open('yelp_academic_dataset_review-wordcount.csv', 'rU') as f:
+    reviews = list(csv.DictReader(f, encoding='utf8'))
+with open('yelp_academic_dataset_business-restaurants.csv', 'rU') as f:
+    businesses = list(csv.DictReader(f, encoding='utf8'))
+
+businesses_by_id = {b['business_id']: b for b in businesses}
+reviews_by_business_id = defaultdict(lambda: [])
+for review in reviews:
+    reviews_by_business_id[review['business_id']].append(review)
+
+new_rows = []
+for business_id, business in businesses_by_id.iteritems():
+    for review in reviews_by_business_id.get(business_id, []):
+        joined_row = {}
+        joined_row.update(business)
+        joined_row.update(review)
+        joined_row.pop('business_id')
+        new_rows.append(joined_row)
+
+with open('business_review-merged.csv', 'wb') as f:
+    dw = csv.DictWriter(f, encoding='utf8', fieldnames=sorted(new_rows[0].keys()))
+    dw.writeheader()
+    dw.writerows(new_rows)
--- process_reviews_wordcount.py
+++ process_reviews_wordcount.py
@@ -2,26 +2,22 @@
 from __future__ import print_function
 from tqdm import tqdm
 import unicodecsv as csv
-from collections import defaultdict
 import json
 
-print("Processing yelp_academic_dataset_review.csv into yelp_academic_dataset_review-wordcount.csv...")
-with open('yelp_academic_dataset_review.csv', 'rU') as f_in:
-    dr = csv.DictReader(f_in, encoding='UTF8')
-    fieldnames = dr.fieldnames
-    fieldnames.remove('text')
-    fieldnames.remove('stars')
-    fieldnames.append('text_wordcount')
-    fieldnames.append('review_stars')
-    with open('yelp_academic_dataset_review-wordcount.csv', 'wb') as f_out:
-       dw = csv.DictWriter(f_out, fieldnames=fieldnames, encoding='UTF8') 
-       dw.writeheader()
-       for row in tqdm(dr):
-           # Basic wordcount - how many chunks after splitting by spaces
-           if row['text']:
-               row['text_wordcount'] = len(row['text'].split(' '))
-               row['review_stars'] = row['stars']
-               row.pop('text')
-               row.pop('stars')
-               dw.writerow(row)
+print("Processing yelp_academic_dataset_review.json into yelp_academic_dataset_review-wordcount.csv...")
+output_rows = []
+with open('yelp_academic_dataset_review.json', 'rb') as f_in:
+    for line in tqdm(f_in):
+        review = json.loads(line)
+        text = review['text'] or ''
+        review['text_wordcount'] = len(text.split(' '))
+        review['review_stars'] = review['stars']
+        review.pop('text')
+        review.pop('stars')
+        output_rows.append(review)
+
+with open('yelp_academic_dataset_review-wordcount.csv', 'wb') as f_out:
+    dw = csv.DictWriter(f_out, fieldnames=sorted(output_rows[0].keys()), encoding='utf8')
+    dw.writeheader()
+    dw.writerows(output_rows)
 print("Done!")
